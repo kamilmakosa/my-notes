4d310c635e0d3bbe7a57422d77a464db9a94f6f0
Kamil Mąkosa <kammakosa@gmail.com>
2021-03-15 00:06:29 +0100

    fead: create bootstrapper

---
+++ bootstrapper.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined("ALLOW_INCLUDE"))	die('Access forbidden');
+
+include('functions/functions.php');
+
+define("PATH", '');
+
+session_start();
+
+include('functions/autostart.php');
